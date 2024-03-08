92becd8184bb585fa5af4234e51a8d192802834f
yugaa18 <162329396+yugaa18@users.noreply.github.com>
2024-03-08 21:59:12 +0530

    Update guess the number.java

--- guess the number.java
+++ guess the number.java
@@ -1,57 +1,56 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessTheNumberGame {
-
-    public static void main(String[] args) {
-        playGame();
-    }
-
-    public static void playGame() {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int lowerBound = 1;
-        int upperBound = 100;
-        int maxAttempts = 10;
-        int score = 0;
-
-        System.out.println("Welcome to the Guess the Number Game!");
-
+public class NumberGame{
+    public static void main(String args []) {
+        Random random= new Random();
+        Scanner sc= new Scanner(System.in);
+        int maxAttempts=10;
+        int score=0;
+        System.out.println("***🎰Welcome to our Number Guessing Game🎰***");
+        System.out.println("-------------------------------------------------");
+        
         do {
-            int generatedNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int x = random.nextInt(100);
             int attempts = 0;
             boolean guessedCorrectly = false;
+            System.out.println("NOTE📝: you will get only 10 attempts to guess the correct number");
+            System.out.println("Guess the number between 0-100.");
+            while (attempts <= maxAttempts && !guessedCorrectly) {
+                System.out.println("enter your guess:");
+                int guess = sc.nextInt();
+                attempts++;
 
-            System.out.println("\nI have generated a number between " + lowerBound + " and " + upperBound +
-                    ". Can you guess it?");
-
-            while (attempts < maxAttempts) {
-                System.out.print("Enter your guess (Attempt " + (attempts + 1) + "): ");
-                int userGuess = scanner.nextInt();
-
-                if (userGuess == generatedNumber) {
-                    System.out.println("Congratulations! You guessed the correct number: " + generatedNumber);
+                if (x == guess) {
+                    System.out.println("you guessed it right👍");
                     guessedCorrectly = true;
-                    break;
-                } else if (userGuess < generatedNumber) {
-                    System.out.println("Too low. Try again!");
+                    score++;
+                    System.out.println("number of attempts is: " +attempts);
+                } else if (x < guess) {
+                    System.out.println("your guess is too high");
                 } else {
-                    System.out.println("Too high. Try again!");
+                    System.out.println("your guess is too low");
                 }
 
-                attempts++;
             }
-
             if (!guessedCorrectly) {
-                System.out.println("Sorry, you've run out of attempts. The correct number was: " + generatedNumber);
-            } else {
-                score += maxAttempts - attempts;
+                System.out.println("sorry!☹️,you ran out of attempts. The correct answer was :" + x);
             }
-
-            System.out.print("Do you want to play again? (yes/no): ");
-        } while (scanner.next().equalsIgnoreCase("yes"));
-
-        System.out.println("\nThank you for playing! Your total score is: " + score);
-        scanner.close();
+            System.out.println("Do you want to play again ? yes/no :");
+            String playAgain = sc.next();
+            if (playAgain.equalsIgnoreCase("no")) {
+                System.out.println("Thanks for Playing!!");
+                System.out.println("Hope you enjoyed the Game😊");
+                break;
+            }
+            else if(playAgain.equalsIgnoreCase("yes")){
+                continue;
+            }
+            else {
+                System.out.println("invalid input");
+                System.out.println("Thanks for Playing!!");
+                break;
+            }
+        } while(true);
     }
 }
